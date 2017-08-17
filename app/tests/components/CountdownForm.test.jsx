5f72679d9f0337ee6c0a4d73e98ff30fcbc6fca4
Sami Kangas <zumartic@gmail.com>
2017-08-17 14:04:07 +0300

    Add countdown functionality

--- app/tests/components/CountdownForm.test.jsx
+++ app/tests/components/CountdownForm.test.jsx
@@ -22,12 +22,45 @@ describe('CountdownForm', () => {
         expect(spy).toHaveBeenCalledWith(109);
     });
 
+    it('should call onSetCountDown if valid seconds entered', () => {
+        var spy = expect.createSpy();
+        var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+        var $el = $(ReactDOM.findDOMNode(countdownForm));
+
+        countdownForm.refs.seconds.value = '59';
+        TestUtils.Simulate.submit($el.find('form')[0]);
+
+        expect(spy).toHaveBeenCalledWith(59);
+    });
+
     it('should not call onSetCountDown if invalid seconds entered', () => {
         var spy = expect.createSpy();
         var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
         var $el = $(ReactDOM.findDOMNode(countdownForm));
 
         countdownForm.refs.seconds.value = '109a';
+        TestUtils.Simulate.submit($el.find('form')[0]);
+
+        expect(spy).toNotHaveBeenCalled();
+    });
+
+     it('should not call onSetCountDown if invalid seconds entered', () => {
+        var spy = expect.createSpy();
+        var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+        var $el = $(ReactDOM.findDOMNode(countdownForm));
+
+        countdownForm.refs.seconds.value = '10x9';
+        TestUtils.Simulate.submit($el.find('form')[0]);
+
+        expect(spy).toNotHaveBeenCalled();
+    });
+
+     it('should not call onSetCountDown if invalid seconds entered', () => {
+        var spy = expect.createSpy();
+        var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+        var $el = $(ReactDOM.findDOMNode(countdownForm));
+
+        countdownForm.refs.seconds.value = 'a109';
         TestUtils.Simulate.submit($el.find('form')[0]);
 
         expect(spy).toNotHaveBeenCalled();
